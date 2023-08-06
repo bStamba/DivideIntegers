d7f012decc59843252d3ef0bc14f674fac9fa3f2
Brandon Stamba <brandonstamba@gmail.com>
2023-08-06 18:55:44 -0500

    Dividing Integers w/o Multi/Divide/Modulus Version 2

--- src/DivideInts.java
+++ src/DivideInts.java
@@ -3,49 +3,65 @@ public class DivideInts {
     //or modulus. So we're going to subtract the divisor from the dividend until
     //the dividend is 0 or less than 0 and then return the total number of times subtracted
     //as the answer for the division.
-    public int divide(int dividend, int divisor){
+    public int divide(int dividend, int divisor) {
         int total = 0;
         boolean isNegative = false;
 
         //If both dividend and divisor are negative then we want to subtract the negative divisor
         //which is essentially adding a positive integer to the negative dividend integer.
-        if(dividend < 0 && divisor < 0){
-            while((dividend - divisor) <= 0){
+        if (dividend < 0 && divisor < 0) {
+            while ((dividend - divisor) <= 0) {
                 dividend = dividend - divisor;
                 total++;
+                if (total == Integer.MAX_VALUE) {
+                    System.out.println("Total is: " + Integer.MAX_VALUE);
+                    return Integer.MAX_VALUE;
+                }
             }
-            System.out.println("Total: " + total);
+            System.out.println("Total is: " + total);
             return total;
         }
         //If only the divisor is negative, then we want to add the negative which is the same as
         //just subtracting a positive integer.
-        else if(divisor < 0){
-            while((dividend + divisor) >= 0){
+        else if (divisor < 0) {
+            while ((dividend + divisor) >= 0) {
                 dividend = dividend + divisor;
                 total++;
+                if (total == Integer.MAX_VALUE) {
+                    System.out.println("Total is: " + -total);
+                    return -Integer.MAX_VALUE;
+                }
             }
-            total = -total;
-            System.out.println("Total: " + total);
-            return total;
+            System.out.println("Total is: " + total);
+            return -total;
         }
+
         //If only the dividend is negative, then we want to add the negative which is the same as
         //just subtracting a positive integer.
-        else if(dividend < 0){
-            while(dividend + divisor <= 0){
+        else if (dividend < 0) {
+            while (dividend + divisor <= 0) {
                 dividend = dividend + divisor;
                 total++;
+                if (total == Integer.MAX_VALUE) {
+                    System.out.println("Total is: " + -total);
+                    return -Integer.MAX_VALUE;
+                }
             }
-            total = -total;
-            System.out.println("Total: " + total);
-            return total;
+            System.out.println("Total is: " + total);
+            return -total;
         }
+
         //If they are both positive, then we will just subtract normally and return the total.
-        else{
-            while((dividend - divisor) >= 0){
+        else {
+            while ((dividend - divisor) >= 0) {
                 dividend = dividend - divisor;
                 total++;
+                if (total == Integer.MAX_VALUE) {
+                    System.out.println("Total is: " + Integer.MAX_VALUE);
+                    return Integer.MAX_VALUE;
+                }
             }
-            System.out.println("Total: " + total);
+            System.out.println("Total is: " + total);
             return total;
         }
     }
--- src/DivideIntsMain.java
+++ src/DivideIntsMain.java
@@ -7,6 +7,13 @@ public class DivideIntsMain {
         di.divide(-60,5);
         di.divide(49, -3);
         di.divide(-49, -3);
-
+        //di.divide(Integer.MAX_VALUE, 1);
+        //di.divide(Integer.MIN_VALUE, 1);
+        //di.divide(Integer.MIN_VALUE, -1);
+        //di.divide(Integer.MAX_VALUE, -1);
+        di.divide(Integer.MAX_VALUE, 1);
+        di.divide(Integer.MAX_VALUE, -1);
+        di.divide(-2147483648, -1);
+        di.divide(-2147483648, 1);
     }
 }
